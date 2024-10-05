90b5cc829ee54853daa724a146777b0b05fdd83f
kylaochweri <k.ochweri@alustudent.com>
2024-10-05 20:54:26 +0300

    0 setup web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# Sets up web servers for deployment of web_static
+#This the scripts code
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx if not already installed
-sudo apt-get update -y
-sudo apt-get install -y nginx
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file to test Nginx configuration
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -17,17 +18,10 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link, force (-f) remove it if it exists
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve the content from the symbolic link
-# The alias directive is used to map /hbnb_static to /data/web_static/current/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-# Restart Nginx to apply changes
-sudo service nginx restart
 
-# Ensure script exits successfully
-exit 0
+sudo service nginx restart
